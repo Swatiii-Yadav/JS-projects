792d5cae61d1a973efe3a3ad56e2d1015aa62ef5
swati yadav <swatiyadav6872@gmail.com>
2025-02-18 17:03:50 +0530

    february 17,18  js Programs

---
+++ JspiderWeb/JsBasics/DataType.js
@@ -0,0 +1,15 @@
+//Data type
+let a=20;
+console.log(typeof a);
+let s="Str";
+console.log(typeof s);
+let c=true;
+console.log(typeof c);
+let d=undefined;
+console.log(typeof d);
+let n=null;
+console.log(n);
+let b=1n;
+console.log(b);
+let g=Symbol("id");
+console.log(typeof g);
