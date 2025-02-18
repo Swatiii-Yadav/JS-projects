f0439fec50ca16a116e4711a70f03e5c82b56f8e
swati yadav <swatiyadav6872@gmail.com>
2025-02-18 16:53:11 +0530

    first commit

---
+++ JspiderWeb/Operator.js
@@ -0,0 +1,10 @@
+//operators
+//Arithmatic Operators
+console.log(2+3);
+console.log(20-3);
+console.log(20*3);
+console.log(20/3);
+console.log(20%3);
+console.log(3%20);
+console.log(20**3); 
+console.log(20**0.5);
